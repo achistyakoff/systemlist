f565b419bf17a0db50f85101678b6992be8c8327
Artem <a.chistyakov@globus.ru>
2023-06-19 20:21:05 +0300

    change

--- server/backend/server.py
+++ server/backend/server.py
@@ -43,6 +43,78 @@ def create_table_if_not_exists():
 # Call the function to check the existence of the 'servers' table and create it if it doesn't exist
 create_table_if_not_exists()
 
+# Check for table existence and create it
+def create_possible_fields_table():
+    connection = create_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS possible_fields ("
+        "id SERIAL PRIMARY KEY,"
+        "field_name VARCHAR(255) UNIQUE)"
+    )
+    
+    # Insert the field names into the table
+    fields = ['environment', 'solution', 'system', 'os', 'tags']
+    for field in fields:
+        cursor.execute(
+            "INSERT INTO possible_fields (field_name) VALUES (%s)",
+            (field,)
+        )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+create_possible_fields_table()
+
+# Function to update server by hostname
+def update_server_by_hostname(hostname, data):
+    connection = create_connection()
+    cursor = connection.cursor()
+    
+    # Validate the fields against the possible_fields table
+    fields_to_validate = ['environment', 'solution', 'system', 'os', 'tags']
+    for field in fields_to_validate:
+        if field not in data:
+            return jsonify({'error': f'Missing field: {field}'}), 400
+        
+        # Perform any additional validation or checks if needed
+        
+    try:
+        # Check if the server exists
+        cursor.execute("SELECT COUNT(*) FROM servers WHERE hostname = %s", (hostname,))
+        count = cursor.fetchone()[0]
+        if count == 0:
+            return jsonify({'error': f'Server with hostname {hostname} does not exist'}), 404
+        
+        # Update the server in the database
+        query = "UPDATE servers SET environment = %s, solution = %s, system = %s, os = %s, tags = %s WHERE hostname = %s"
+        cursor.execute(query, (data['environment'], data['solution'], data['system'], data['os'], data['tags'], hostname))
+        connection.commit()
+        
+        return jsonify({'message': f'Server with hostname {hostname} updated successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
+
+# Add a route to fetch the values from the table via a GET request:
+@app.route('/api/possible-fields', methods=['GET'])
+def get_possible_fields():
+    connection = create_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT field_name FROM possible_fields")
+    fields = [field[0] for field in cursor.fetchall()]
+    
+    cursor.close()
+    connection.close()
+
+    return jsonify(fields)
+
+
 # Route to get all servers
 @app.route('/api/servers', methods=['GET'])
 def get_servers():
@@ -90,7 +162,7 @@ def get_server_by_hostname(hostname):
         return jsonify({'error': 'Server not found'}), 404
 
 # Route for creating records in the table with hostname validation
-@app.route('/servers', methods=['POST'])
+@app.route('/api/servers', methods=['POST'])
 def create_server():
     data = request.json
     hostname = data.get('hostname')
@@ -119,33 +191,13 @@ def create_server():
 
     return jsonify({'message': 'Server created successfully'}), 201
 
+
 # Route to update server by hostname
-@app.route('/api/servers/<hostname>', methods=['PUT'])
-def update_server_by_hostname(hostname):
-    connection = create_connection()
-    cursor = connection.cursor()
-    query = "SELECT * FROM servers WHERE hostname = %s"
-    cursor.execute(query, (hostname,))
-    server = cursor.fetchone()
-    if server:
-        new_data = request.get_json()
-        update_query = "UPDATE servers SET environment = %s, solution = %s, system = %s, os = %s, tags = %s WHERE hostname = %s"
-        cursor.execute(update_query, (
-            new_data['environment'],
-            new_data['solution'],
-            new_data['system'],
-            new_data['os'],
-            new_data['tags'],
-            hostname
-        ))
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return jsonify({'message': 'Server updated successfully'})
-    else:
-        cursor.close()
-        connection.close()
-        return jsonify({'error': 'Server not found'}), 404
+@app.route('/api/servers/<string:hostname>', methods=['PUT'])
+def update_server(hostname):
+    data = request.get_json()
+    return update_server_by_hostname(hostname, data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
